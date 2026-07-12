c33501204d4da60896e28c685b72c44c7a2f0f01
Jon Mantooth <jonmantooth@gmail.com>
2026-07-12 14:51:57 -0400

    Initial Commit

---
+++ HelloSwift/HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftApp.swift
+//  HelloSwift
+//
+//  Created by jon mantooth on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
